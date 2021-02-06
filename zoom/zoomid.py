e57708dc2a91ee597aad9a5abe211b22dadb8b7a
NooberrUwU <65776978+NooberrUwU@users.noreply.github.com>
2021-02-06 07:43:37 +0700

    v1.3.3

--- zoom/zoomid.py
+++ zoom/zoomid.py
@@ -20,15 +20,13 @@ class zoomid(commands.Cog):
 
 		embedContent2 = discord.Embed(color=discord.Colour.from_rgb(255,182,193))
 		embedContent2.add_field(name="Môn học", value="1) Toán\n2) Ngữ văn\n3) Tiếng Anh\n4) Lịch sử\n5) GDCD\n6) Địa lý\n7) Sinh học\n8) Hóa học\n9) Vật lý\n10) Công nghệ\n11) Âm nhạc\n12) Thể dục\n13) Mỹ thuật\n14) Tin học", inline=True)
-		embedContent2.add_field(name="Meeting IDs‏‏‎", value="3345081082\n5320894930\n6472181785\n3894189011\n8102536065\n4636921326\n9837833699\n6253175884\n6570777853\n2686024835\n71887586251\n~\n~\n~", inline=True)
-		embedContent2.add_field(name="Passcode‏‏‎s", value="1234567\n825648\n123456\n906409\n493755\n123\n9999\n888888\n004546\n766848\n210055\n~\n~\n~", inline=True)
+		embedContent2.add_field(name="Meeting IDs", value="3345081082\n5320894930\n6472181785\n3894189011\n8102536065\n4636921326\n9837833699\n6253175884\n6570777853\n2686024835\n71887586251\n~\n~\n~", inline=True)
+		embedContent2.add_field(name="Passcodes", value="1234567\n825648\n123456\n906409\n493755\n123\n9999\n888888\n004546\n766848\n210055\n~\n~\n~", inline=True)
 		message2 = await ctx.send(embed=embedContent2)
 
-		time.sleep(30)
-		# await message1.delete()
-		messageX = ctx.m
-		await messageX.delete()
-		await message2.delete()
+# 		time.sleep(30
+# 		await messageX.delete()
+# 		await message2.delete()
 
 def setup(client):
 	client.add_cog(zoomid(client))
